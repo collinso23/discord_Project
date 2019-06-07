4c6f75a16a4496fc97dd1bb924466570445ad5fb
eneadodi <enea.dodi98@gmail.com>
2019-06-07 18:34:01 -0400

    Almost there

--- wiki_scrape_obj.py
+++ wiki_scrape_obj.py
@@ -7,7 +7,7 @@ class Wiki_Scraper(object):
         self.HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
     def getClassInformation(self,className):
-        url = "https://www.dandwiki.com/wiki/5e_Classes"
+        url = "https://www.dandwiki.com/wiki/5e_SRD:Classes"
         page = requests.get(url,headers=self.HEADERS).text
         soup = BeautifulSoup(page,features ="lxml")
         mainDiv = soup.find('div',{'class': 'mw-parser-output'})
@@ -22,7 +22,33 @@ class Wiki_Scraper(object):
                 if className in title:
                     print(title)
 
+    def getRaceInformation(self,raceName):
+        url = "https://www.dandwiki.com/wiki/5e_SRD:Races"
+        page = requests.get(url, headers=self.HEADERS).text
+        soup = BeautifulSoup(page,features="lxml")
+        mainTable = soup.find('table',{'class':'5e mw-collapsible'})
+        mainDiv = soup.find('div',{'class','mw-parser-output'})
+        rows = mainTable.findAll('tr')
+        #print(rows)
+        #links = rows.findAll('a')
+        links = mainDiv.findAll('a')
+        #print(links)
+        for link in links:
+            title = link.get('title')
+            if title is not None:
+                if raceName in title:
+                    self.extractFromRacePage(link['href'])
+
+    def extractFromRacePage(self,url):
+        page = requests.get(url,headers=self.HEADERS).text
+        soup = BeauitfulSoup(page,features="lxml")
+        mainDiv = soup.find('div',{'class','mw-parser-output'})
+        allParagraphs = mainDiv.findAll('p')
+        print(allParagraphs)
+
+        #for row, tr in enumerate
 wc = Wiki_Scraper()
 wc.getClassInformation("Warlock")
+wc.getRaceInformation("Human")
     
 
