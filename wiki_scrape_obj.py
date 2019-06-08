06c4010bc09e0a998e2e9159c404b52149be43d8
eneadodi <enea.dodi98@gmail.com>
2019-06-08 15:24:58 -0400

    Race Searcher Not really but odne

--- wiki_scrape_obj.py
+++ wiki_scrape_obj.py
@@ -1,15 +1,25 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import lxml
 
 class Wiki_Scraper(object):
     def __init__(self):
         self.HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
-
-    def getClassInformation(self,className):
-        url = "https://www.dandwiki.com/wiki/5e_SRD:Classes"
+    """
+    param: url
+    return the Entire HTML code fo the website
+    """
+    def getEntireHTMLPage(self,url):
         page = requests.get(url,headers=self.HEADERS).text
         soup = BeautifulSoup(page,features ="lxml")
+        return soup
+    """
+    param:className
+    returns the class information of the character
+    Cannot Handle: Incorrectly spelled name
+    """
+    def getClassInformation(self,className):
+        soup = self.getEntireHTMLPage("https://www.dandwiki.com/wiki/5e_SRD:Classes")
         mainDiv = soup.find('div',{'class': 'mw-parser-output'})
         allLinks = mainDiv.findAll('a')
         #table = soup.find('table', {'class': 'column'})
@@ -23,9 +33,7 @@ class Wiki_Scraper(object):
                     print(title)
 
     def getRaceInformation(self,raceName):
-        url = "https://www.dandwiki.com/wiki/5e_SRD:Races"
-        page = requests.get(url, headers=self.HEADERS).text
-        soup = BeautifulSoup(page,features="lxml")
+        soup = self.getEntireHTMLPage("https://www.dandwiki.com/wiki/5e_SRD:Races")
         mainTable = soup.find('table',{'class':'5e mw-collapsible'})
         mainDiv = soup.find('div',{'class','mw-parser-output'})
         rows = mainTable.findAll('tr')
@@ -38,17 +46,37 @@ class Wiki_Scraper(object):
             if title is not None:
                 if raceName in title:
                     self.extractFromRacePage(link['href'])
+                    #print(link['href'])
 
-    def extractFromRacePage(self,url):
-        page = requests.get(url,headers=self.HEADERS).text
-        soup = BeauitfulSoup(page,features="lxml")
+    
+    def extractFromRacePage(self,href):
+        soup = self.getEntireHTMLPage("https://www.dandwiki.com" + href)
         mainDiv = soup.find('div',{'class','mw-parser-output'})
         allParagraphs = mainDiv.findAll('p')
-        print(allParagraphs)
+        allText = [p.find_all(text=True,recursive=False) for p in allParagraphs]
+        #returnText
 
+        """ for index in allText:
+            if len(index.text) > 3:
+                returnText = returnText + index.text
+
+        print(returnText)"""
+        print(allText)
+        returnText = ""
+        dividedText = ''.join(map(str, allText))
+        for l in dividedText:
+            if len(l) > 1:
+                for sentence in l:
+                    returnText = returnText + sentence
+            else:
+                returnText = returnText + "\n"
+        #print(returnText)
+        #print(''.join(allText))
+        #print('\n'.join(allTextList)) 
         #for row, tr in enumerate
 wc = Wiki_Scraper()
-wc.getClassInformation("Warlock")
-wc.getRaceInformation("Human")
-    
+race = input("What Race should I search for? ")
+#print("What Race should I search For?")
+#wc.getClassInformation("Warlock")
+wc.getRaceInformation(race)
 
