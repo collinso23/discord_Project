5f93e928a1e08624fb22d514d6e6121f10f685b4
Orion Collins <collinso@wit.edu>
2019-06-13 19:47:43 -0400

    more debugging

--- inqBot/cogs/greetings.py
+++ inqBot/cogs/greetings.py
@@ -1,19 +1,18 @@
 import discord
-from discord.ext.commands import Bot,Cog
+from discord.ext.commands
 """example cog to understand cog implementation so that search may be later implemented as a cog"""
-class Greetings(Cog):
-    def __init__(self,bot:Bot):
+class Greetings(commands.Cog):
+    def __init__(self,bot):
             self.bot = bot
             self._last_member = None
 
-    @Cog.listener()
+    @commands.Cog.listener()
     async def on_member_join(self,member):
-        channel = member.guild.system_channel
-        if channel is not None:
-            await channel.send("Welcome {0.mention}.".format(member))
+        """Greet Members when they join the server"""
+        await self.bot.say('{0.name}' joined '{0.joined_at}'.format(member))
 
-    @Bot.command()
-    async def hello(self,ctx,*,member: discord.Memeber = None):
+    @commands.command()
+    async def hello(self,ctx,*,member: discord.Memeber=None):
         """ Says Hello"""
         member = member or ctx.author
         if self._last_memer is None or self._last_member.id != member.id:
@@ -21,5 +20,6 @@ class Greetings(Cog):
         else:
             await ctx.send("Hello {0.name}...This feels familiar".format(member))
         self._last_memeber = member
+
 def setup(bot):
     bot.add_cog(Greetings(bot))
--- inqBot/inq_bot.py
+++ inqBot/inq_bot.py
@@ -4,30 +4,68 @@ import logging
 
 from discord import Game
 
+from discord.ext import commands
+
+from discord.ext.commands import Cog, Bot, when_mentioned_or
+
 from log import DiscordHandler
 
-from discord.ext.commands import Bot, when_mentioned_or
-
 from utils import permissions, default
+desc="""A bot to help with the running of DND campaigns
+    Along with general administrative functions"""
 
 config = default.get("config.json")
 
 logger = logging.getLogger(__name__)
 
+startup_extensions=['greetings']
+
 bot = Bot(
     command_prefix=when_mentioned_or(config.prefix),
+    
+    description=desc,
 
     activity=Game(name=":help")
 )
 
 logger.addHandler(DiscordHandler(bot))
 logger.setLevel(logging.INFO)
-
 bot.log = logger
 
+
+@bot.event
+async def on_ready():
+    print('Logged in as {} {}'.format(bot.user.name,bot.user.id))
+    print('------')
+
+@bot.command()
+async def load(extension_name:str):
+    """Loads Extensions """
+    try:
+        bot.load_extension(extension_name)
+    except(AttributeError, ImportError) as err:
+        await bot.say("```py\n{}: {}\n```".format(type(err).__name__, str(err)))
+        return
+    await bot.say("{} loaded.".format(extension_name))
+
+@bot.command()
+async def unload(extension_name:str):
+    """Unload Extensions"""
+    bot.unload_extension(extension_name)
+    await bot.say("{} unloaded".format(extension_name))
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as err:
+            exc = '{}: {}'.format(type(err).__name__,err)
+            print('Failed to load extension {}\n{}'.format(extension,exc))
+"""
 # Load cogs
 for file in os.listdir("cogs"):
     if file.endswith(".py"):
         name = file[:-3]
         bot.load_extension(f"cogs.{name}")
+"""        
 bot.run(config.token)
