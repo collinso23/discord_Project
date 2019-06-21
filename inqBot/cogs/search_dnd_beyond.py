dd95a4e51c4580f1077a38e51d6fe1d2a3151bdd
Orion Collins <collinso@wit.edu>
2019-06-21 00:28:28 -0400

    first function scrape!

--- inqBot/cogs/search_dnd_beyond.py
+++ inqBot/cogs/search_dnd_beyond.py
@@ -1,65 +1,15 @@
 import discord
 from discord.ext import commands
-from utils import default, dnd_beyond_scrape,repo
-from bs4 import BeautifulSoup, SoupStrainer, Tag
-import requests
-import lxml
-import re
-import urllib
+from utils import default, dnd_scraper, repo
 import textwrap
-from nltk import tokenize
 
 class Search_DnD(commands.Cog):
+
         def __init__(self,bot):
             self.bot = bot
-            self.HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
-            #self.scraper = dnd_beyond_scrape.DnD_Scraper()
-
-        """
-        param: url
-        return the Entire HTML code fo the website
-        """
-        def getEntireHTMLPage(self,url):
-            page = requests.get(url,headers=self.HEADERS).text
-            soup = BeautifulSoup(page,features ="lxml")
-            return soup
-
-        """
-        breakTextByLength method
-        breaks text up by length we would like on each line
-        If word exceeds length, then new line is processed before
-        """
-        def breakTextByLength(self,text,length = 150):
-            if text is not None:
-                text_by_line = re.split('[\r\n]',text)
-                return_String = ""
-                for t in text_by_line:
-                    if t is not None:
-                        if len(t) > 149:
-                            dividedText = textwrap.wrap(t,length)
-                            dividedText = '\n'.join(dividedText)
-                            return_String = return_String + dividedText + '\n'
-                        else:
-                            return_String = return_String + t + '\n'
-                return return_String
-        """
-        titlize method
-        Gives a text title format
-        most words are capitalized, simply words like -> is,of,in,a,an,the, etc are not
-        """
-        def titlize(self, text):
-            splitText = text.split()
-            specialCaseWords = ["from","so","is","of","in","a","an","the","but","for"]
-            return_String = []
-            for w in splitText:
-                if w not in specialCaseWords:
-                    return_String.append(w.capitalize())
-                else:
-                    return_String.append(w)
-            return ' '.join(map(str, return_String))
 
         @commands.group(pass_context=True)
-        async def tomes(self,ctx):
+        async def tomes(self,ctx,*args):
             """
             Scrapes DND beyond: https://www.dndbeyond.com/search?q= for various dnd related
             features and returns them to the chat so that users may quickly resume
@@ -70,76 +20,24 @@ class Search_DnD(commands.Cog):
 
             Command syntax: $tomes --subcommand 'name_of_search' 'specifier'
             """
-            if ctx.invoked_subcommand is None:
-                await ctx.send_help(str(ctx.command))
+            scraper = dnd_scraper.DnD_Scraper()
+            arguments = []
+            for a in args:
+                arguments.append(a)
+
+            arguments_as_string = " ".join(arguments)
+            rstring = scraper.searchDNDWebsite(arguments[0],arguments[1],arguments[2])
+            rstring_as_list = textwrap.wrap(rstring, 1500, break_long_words=False)
+
+            for message in rstring_as_list:
+                await ctx.send(message)
+            #if ctx.invoked_subcommand is None:
+            #    await ctx.send_help(str(ctx.command))
 
         @tomes.command(name="--race",aliases=["-r"])
         async def _tomesRace(self,ctx,name_of_search,specifier=""):
-            #scraper = dnd_beyond_scrape.DnD_Scraper()
-            name_of_search = re.sub('[^\w]', '%20', name_of_search)
-            url = "https://www.dndbeyond.com/search?q="+name_of_search+"&f=backgrounds,classes,feats,races&c=characters"
-            soup = self.getEntireHTMLPage(url)
-            soup_2 = self.searchRace(soup,specifier)
-            self.extractInformationFromRacePage(soup_2,specifier)
 
-            await ctx.send("Here!!: {}".format(type(info)))
+            await ctx.send("Here!!: {}, {}".format(name_of_search,specifier))
 
-        """
-        param: raceName
-            Name of race to be searched
-        """
-        def searchRace(self, soup, specifier):
-            searchListing = soup.find('div',{'class':'ddb-search-results-listing'})
-            searches = soup.find_all('div',{'class':'ddb-search-results-listing-item'})
-            for search in searches:
-                if search is not None:
-                    if search.find('span', {'class':'races'}) is not None:
-                        link = search.find('a')['href']
-                        url = "https://www.dndbeyond.com" + link
-                        soup = self.getEntireHTMLPage(url)
-                        return soup
-                        #self.extractInformationFromRacePage(soup,specifier)
-                        #break
-
-        "extacts information from Race Page"
-        def extractInformationFromRacePage(self,soup,specifier):
-            mainInformation = soup.find('div',{'id','content'})
-            headers = soup.find_all("h2")
-            info = ""
-            allInfo = ""
-            for header in headers:
-                if header is not None:
-                    if specifier in header.text:
-                        nextNode = header
-                        info = ""
-                        """/42820342/get-text-in-between-two-h2-headers-using-beautifulsoup"""
-                        while True:
-                            nextNode = nextNode.nextSibling
-                            if nextNode is None:
-                                break
-                            if isinstance(nextNode, Tag):
-                                if nextNode.name == "span":
-                                    info = info + self.breakTextByLength(nextNode.text) + "\n"
-                                if nextNode.name == "h4":
-                                    info = info + nextNode.text + "\n"
-                                elif nextNode.name == "h2":
-                                    break
-                                elif nextNode.name == "h3":
-                                    break
-                        allInfo = allInfo + info + "\n"
-            return allInfo
-        """
-        @commands.command()
-        async def tomes(self,ctx,section,name_of_search,specifier=""):
-            scraper = dnd_beyond_scrape.DnD_Scraper()
-            if section =="--class":
-                race_info = scraper.searchDNDWebsite(section,name_of_search,specifier)
-            elif section == "--items":
-                item_info = scraper.searchDNDWebsite(section,name_of_search,specifier)
-                #return_string = scraper.extractInformationFromItemPage(item_info)
-                await ctx.send("THE TOMES SPEAK {}".format(type(item_info)))
-            #if ctx.invoked_subcommand is None:
-            #    await ctx.send_help(str(ctx.command))
-        """
 def setup(bot):
     bot.add_cog(Search_DnD(bot))
---
+++ inqBot/utils/dnd_scraper.py
@@ -0,0 +1,296 @@
+from bs4 import BeautifulSoup, SoupStrainer, Tag
+import requests
+import lxml
+import re
+import urllib
+import textwrap
+from nltk import tokenize
+
+"""
+Scrapes from the offical Dnd Page
+"""
+class DnD_Scraper(object):
+
+    """Constructor
+    """
+    def __init__(self):
+        self.HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+
+
+    """
+    param: url
+    return the Entire HTML code fo the website
+    """
+    def getEntireHTMLPage(self,url):
+        page = requests.get(url,headers=self.HEADERS).text
+        soup = BeautifulSoup(page,features ="lxml")
+        return soup
+
+    """
+    """
+    def requestIdentifier(self, command, section, nameOfSearch, specifier):
+        if command == "$search":
+            self.searchDNDWebsite(command, section, specifier)
+        """
+        elif command == "$SOMETHINGELSE":
+            #self.search()
+        else:
+            #return "Not an actual command"
+        """
+    """
+    param: section, nameOfSearch
+        section: specifies if user is looking for imformation about items, race, class, etc.
+        nameOfSearch: what to be searched for on the search bar
+    If user calls the search function on the bot, then the bot will perofrm a search on
+    the search bar on dndbeyond according to the requested section
+    """
+    def searchDNDWebsite(self, section, nameOfSearch,specifier=""):
+        nameOfSearch = re.sub('[^\w]', '%20', nameOfSearch)
+        url = "https://www.dndbeyond.com/search?q=" + nameOfSearch
+        rstring = ""
+        print(url)
+        soup = self.getEntireHTMLPage(url)
+        if section == "item":
+            soup = self.getEntireHTMLPage(url+"&f=equipment,magic-items&c=items")
+            rstring = self.searchItem(soup,1)
+        elif section == "equipment":
+            soup = self.getEntireHTMLPage(url+"&f=equipment,magic-items&c=items")
+            rstring = self.searchItem(soup,2)
+        elif section == "class":
+            soup = self.getEntireHTMLPage(url+"&f=backgrounds,classes,feats,races&c=characters")
+            rstring = self.searchClass(soup,specifier)
+        elif section == "race":
+            soup = self.getEntireHTMLPage(url+"&f=backgrounds,classes,feats,races&c=characters")
+            rstring = self.searchRace(soup,specifier)
+        elif section == "spell":
+            soup = self.getEntireHTMLPage(url+"&f=spells&c=spells")
+            rstring = self.searchSpell(soup,nameOfSearch)
+        return rstring
+
+    """
+    param: itemName
+        name of the Item to be searched
+    """
+    def searchItem(self, soup, kind):
+        searchListing = soup.find('div',{'class':'ddb-search-results-listing'})
+        searches = soup.find_all('div',{'class':'ddb-search-results-listing-item'})
+        rstring = ""
+        for search in searches:
+            if search is not None:
+                if kind == 1:
+                    if search.find('span', {'class':'magic-items'}) is not None:
+                        link = search.find('a')['href']
+                        url = "https://www.dndbeyond.com" + link
+                        soup = self.getEntireHTMLPage(url)
+                        return self.extractInformationFromItemPage(soup)
+                        break
+                elif kind == 2:
+                    if search.find('span', {'class':'equipment'}) is not None:
+                        link = search.find('a')['href']
+                        url = "https://www.dndbeyond.com" + link
+                        soup = self.getEntireHTMLPage(url)
+                        return self.extractInformationFromEquipmentPage(soup)
+                        break
+
+        return "Nothing was caught"
+
+    """
+    Extracts information about armor from the offical page
+    """
+    def extractInformationFromEquipmentPage(self,soup):
+        mainInformation = soup.find('div',{'class','details-container-content-description'})
+        return self.breakTextByLength(mainInformation.text)
+
+    def extractInformationFromItemPage(self,soup):
+        mainInformation = soup.find('div',{'class','more-info-content'})
+        return self.breakTextByLength(mainInformation.text)
+
+    """
+    param: soup, specifier
+    soup of search page
+    specifier is what you are specifically looking for
+    Name of class to be searched
+    """
+    def searchClass(self, soup, specifier):
+        searchListing = soup.find('div',{'class':'ddb-search-results-listing'})
+        searches = soup.find_all('div',{'class':'ddb-search-results-listing-item'})
+        for search in searches:
+            if search is not None:
+                if search.find('span', {'class':'classes'}) is not None:
+                    link = search.find('a')['href']
+                    url = "https://www.dndbeyond.com" + link
+                    soup = self.getEntireHTMLPage(url)
+                    return self.extractInformationFromClassPage(soup,specifier)
+                    break
+        return "Nothing was caught"
+
+    """Extracts from class page and a subsection being the specifier"""
+    def extractInformationFromClassPage(self, soup, specifier):
+        mainInformation = soup.find('div',{'id','content'})
+        bigHeaders = soup.find_all(re.compile('^h[1-6]$'))
+        smallHeaders = soup.find_all('dl')
+        headers = bigHeaders+smallHeaders
+        info = ""
+        allInfo = ""
+        for header in headers:
+            if header is not None:
+                if specifier in header.text:
+                    nextNode = header
+                    info = ""
+
+                    """/42820342/get-text-in-between-two-h2-headers-using-beautifulsoup"""
+                    while True:
+                        nextNode = nextNode.nextSibling
+                        if nextNode is None:
+                            break
+                        if isinstance(nextNode, Tag):
+                            if nextNode.name == "p":
+                                info += self.breakTextByLength(nextNode.text)
+                            else:
+                                break
+                    allInfo = allInfo + info + "\n"
+        return allInfo
+
+
+    """
+    param: raceName
+        Name of race to be searched
+    """
+    def searchRace(self, soup, specifier):
+        searchListing = soup.find('div',{'class':'ddb-search-results-listing'})
+        searches = soup.find_all('div',{'class':'ddb-search-results-listing-item'})
+        for search in searches:
+            if search is not None:
+                if search.find('span', {'class':'races'}) is not None:
+                    link = search.find('a')['href']
+                    url = "https://www.dndbeyond.com" + link
+                    soup = self.getEntireHTMLPage(url)
+                    return self.extractInformationFromRacePage(soup,specifier)
+                    break
+        return "Nothing was caught"
+
+    "extacts information from Race Page"
+    def extractInformationFromRacePage(self,soup,specifier):
+        mainInformation = soup.find('div',{'id','content'})
+        headers = soup.find_all("h2")
+        info = ""
+        allInfo = ""
+        for header in headers:
+            if header is not None:
+                if specifier in header.text:
+                    nextNode = header
+                    info = ""
+                    """/42820342/get-text-in-between-two-h2-headers-using-beautifulsoup"""
+                    while True:
+                        nextNode = nextNode.nextSibling
+                        if nextNode is None:
+                            break
+                        if isinstance(nextNode, Tag):
+                            if nextNode.name == "span":
+                                info = info + self.breakTextByLength(nextNode.text) + "\n"
+                            if nextNode.name == "h4":
+                                info = info + nextNode.text + "\n"
+                            elif nextNode.name == "h2":
+                                break
+                            elif nextNode.name == "h3":
+                                break
+                    allInfo = allInfo + info + "\n"
+        return allInfo
+
+    """
+    param: spellName
+        Name of Spell to be searched
+    """
+    def searchSpell(self, soup,nameOfSearch):
+        searchListing = soup.find('div',{'class':'ddb-search-results-listing'})
+        searches = soup.find_all('div',{'class':'ddb-search-results-listing-item'})
+        for search in searches:
+            if search is not None:
+                if search.find('span', {'class':'spells'}) is not None:
+                    link = search.find('a')['href']
+                    url = "https://www.dndbeyond.com" + link
+                    soup = self.getEntireHTMLPage(url)
+                    return self.extractInformationFromSpellPage(soup,nameOfSearch)
+                    break
+        return "Nothing was caught"
+
+    def extractInformationFromSpellPage(self,soup,nameOfSearch):
+        mainInformation = soup.find('section',{'class','detail-content'})
+        return self.fixSpacingOnText(self.breakTextByLength(mainInformation.text))
+    """
+    If given a list of strings, this method concatenates the lists into
+    a single string and makes it look pretty.
+    """
+    def printListOfText(self,listText):
+        lines = ""
+        for l in listText:
+            lines += l
+        textDividedByLength = self.breakTextByLength(lines)
+        return textDividedByLength
+
+
+    """
+    breakTextIntoSentences method
+    breaks a text by sentences.
+    Beta version, does not take into account all instances
+    """
+    def breakTextIntoSentences(self,text):
+        textBySentence = re.split(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', text)
+        return textBySentence
+
+    """
+    breakTextByLength method
+    breaks text up by length we would like on each line
+    If word exceeds length, then new line is processed before
+    """
+    def breakTextByLength(self,text,length = 150):
+        if text is not None:
+            text_by_line = re.split('[\r\n]',text)
+            rstring = ""
+            for t in text_by_line:
+                if t is not None:
+                    if len(t) > 149:
+                        dividedText = textwrap.wrap(t,length)
+                        dividedText = '\n'.join(dividedText)
+                        rstring = rstring + dividedText + '\n'
+                    else:
+                        rstring = rstring + t + '\n'
+            return rstring
+
+
+    def fixSpacingOnText(self,text):
+        text_by_line = text.split('\n')
+        finaltext = ""
+        for line in text_by_line:
+            if len(line) > 2:
+                line = re.sub(r'^\s+','',line)
+                finaltext = finaltext + line+ "\n"
+        return finaltext
+
+
+
+    """
+    titlize method
+    Gives a text title format
+    most words are capitalized, simply words like -> is,of,in,a,an,the, etc are not
+    """
+    def titlize(self, text):
+        splitText = text.split()
+        specialCaseWords = ["from","so","is","of","in","a","an","the","but","for"]
+        returnString = []
+        for w in splitText:
+            if w not in specialCaseWords:
+                returnString.append(w.capitalize())
+            else:
+                returnString.append(w)
+        return ' '.join(map(str, returnString))
+
+
+"""
+scrap = DnD_Scraper()
+scrap.searchDNDWebsite("item", "potion of healing")
+scrap.searchDNDWebsite("class", "Barbarian","Rage")
+scrap.searchDNDWebsite("race","Human","Human Traits")
+#scrap.searchDNDWebsite("equipment","spiked armor")
+scrap.searchDNDWebsite("spell","fireball
+"""
